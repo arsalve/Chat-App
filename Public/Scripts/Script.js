ee2d89fc641be0f18482e7dcf08c149467ce3181
Ashish Salve <a.r.salve@gmail.com>
2021-04-18 16:49:05 +0530

    new Message on top

--- Public/Scripts/Script.js
+++ Public/Scripts/Script.js
@@ -61,7 +61,7 @@ function load_text() {
 function putMSGdata(Msg) {
     document.querySelector("#sizing-addon3").innerHTML='Updated' + Msg.time ;
     var old = document.querySelector("#output").innerHTML;
-    document.querySelector("#output").innerHTML = old + '<div class="col-sm-12"><table class="form-comments-table"><tr><td ><div class="comment-user" > &nbsp' + Msg.name + '</div></td><td> </td><td style="width:100%;><div id="comment-14" data-commentid="14" class="comment comment-step1"  word-wrap: break-word">' + Msg.data + '</div> </td></tr></table></div">';
+    document.querySelector("#output").innerHTML =  '<div class="col-sm-12"><table class="form-comments-table"><tr><td ><div class="comment-user" > &nbsp' + Msg.name + '</div></td><td> </td><td style="width:100%;><div id="comment-14" data-commentid="14" class="comment comment-step1"  word-wrap: break-word">' + Msg.data + '</div> </td></tr></table></div">'+old ;
 }
 document.addEventListener('BeforeUnload', () => {
     socket.leave(window.location.hash);
